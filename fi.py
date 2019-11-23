683291802e62d86445d6457952578558120b771d
Antti Ajanki <antti.ajanki@iki.fi>
2019-11-23 11:11:01 +0200

    Build lemma lookups without considering the rules. Enable lookups in spacy_fi

--- fi.py
+++ fi.py
@@ -2,7 +2,6 @@ from spacy.lang.fi import FinnishDefaults
 from spacy.language import Language
 from spacy.symbols import POS, PUNCT, SYM, ADJ, CCONJ, SCONJ, NUM, DET, ADV
 from spacy.symbols import ADP, X, VERB, NOUN, PROPN, PART, INTJ, SPACE, PRON
-from voikko_lemmatizer import VoikkoLemmatizer
 
 TAG_MAP = {
     'Adv': {POS: ADV},
@@ -23,11 +22,10 @@ TAG_MAP = {
 
 
 class FinnishExDefaults(FinnishDefaults):
-    @classmethod
-    def create_lemmatizer(cls, nlp=None, lookups=None):
-        return VoikkoLemmatizer(lookups)
-
     tag_map = TAG_MAP
+    resources = [
+        ('lemma_lookup', 'data/lemma/fi_lemma_lookup.json'),
+    ]
 
 
 class FinnishEx(Language):
--- lemma/create_lemma_lookup.py
+++ lemma/create_lemma_lookup.py
@@ -2,61 +2,37 @@ import json
 import re
 import string
 import sys
-import plac
 from collections import OrderedDict
-from itertools import chain, islice
-from pathlib import Path
 from voikko import libvoikko
 
 pure_num_re = re.compile(r'^-?=q+(:p+)$')
 
 
-@plac.annotations(
-    rulefile=('The path to fi_lemma_rules.json', 'positional', None, Path)
-)
-def main(rulefile):
+def main():
     v = libvoikko.Voikko('fi')
     strip_punct = ''.join(x for x in string.punctuation if x != '-')
 
-    rules = load_rules(rulefile)
-
-    lines = (line.strip().split(' ') for line in sys.stdin)
-    word_counts = ((x[1], int(x[0])) for x in lines)
-    word_counts = ((x[0].strip(strip_punct), x[1]) for x in word_counts)
-    frequent_words = (w[0] for w in word_counts if w[1] >= 10)
-    words_with_lemmas = voikko_lemmas(frequent_words, v)
-
-    lookup = OrderedDict(sorted(lemma_exceptions(words_with_lemmas, rules)))
-    json.dump(lookup, fp=sys.stdout, indent=2, ensure_ascii=False)
-
-
-def lemma_exceptions(words_with_lemmas, rules):
-    for word, lemmas in words_with_lemmas:
-        found_match = False
-        for rule in rules:
-            if word.endswith(rule[0]):
-                form = word[:-len(rule[0])] + rule[1]
-                lemma_lower = [lemma.lower() for lemma in lemmas]
-                if form.lower() in lemma_lower:
-                    found_match = True
-                    break
-
-        if not found_match and word.lower() != lemmas[0].lower():
-            yield (word, lemmas[0])
-
-
-def load_rules(rulefile):
-    rules = json.load(rulefile.open())
-    return list(chain.from_iterable(rules.values()))
+    words = (line.strip().split(' ', 1)[1] for line in sys.stdin)
+    words = (x.strip(strip_punct) for x in words)
+    lemmas = voikko_lemmas(words, v)
+    lemmas = OrderedDict(sorted(lemmas))
+    json.dump(lemmas, fp=sys.stdout, indent=2, ensure_ascii=False)
 
 
 def voikko_lemmas(words, v):
     for word in words:
-        analyses = (x for x in v.analyze(word) if x.get('BASEFORM'))
-        analyses = (x for x in analyses if not is_pure_num(x))
-        lemmas = [x.get('BASEFORM') for x in analyses]
-        if lemmas:
-            yield (word, lemmas)
+        analyses = (
+            x for x in v.analyze(word)
+            if x.get('BASEFORM') and not is_pure_num(x)
+        )
+
+        try:
+            lemma = next(analyses).get('BASEFORM')
+        except StopIteration:
+            continue
+
+        if lemma and word.lower() != lemma.lower():
+            yield (word, lemma)
 
 
 def is_pure_num(analysis):
@@ -68,4 +44,4 @@ def is_pure_num(analysis):
 
 
 if __name__ == '__main__':
-    plac.call(main)
+    main()
