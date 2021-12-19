566c3742af99ec26b14e47f06aa0c107c647bdaa
Antti Ajanki <antti.ajanki@iki.fi>
2021-12-19 10:21:32 +0200

    Train NER model on turku-one with a fixed tok2vec

--- tools/mergemodels.py
+++ tools/mergemodels.py
@@ -1,48 +1,70 @@
 import copy
 import json
-import os.path
+import re
 import shutil
-import sys
+import typer
+from pathlib import Path
 
 
-def main():
-    tagger_path, ner_path, merged_path = sys.argv[1:4]
-
-    if os.path.exists(merged_path):
+def main(
+        tagger_path: Path = typer.Argument(..., help='Tagger+parser model directory'),
+        ner_path: Path = typer.Argument(..., help='NER model directory'),
+        merged_path: Path = typer.Argument(..., help='Output directory')
+):
+    if merged_path.exists():
         shutil.rmtree(merged_path)
-    os.makedirs(merged_path)
 
-    tagger_meta = json.load(open(os.path.join(tagger_path, 'meta.json')))
-    ner_meta = json.load(open(os.path.join(ner_path, 'meta.json')))
+    shutil.copytree(tagger_path, merged_path)
+    shutil.copytree(ner_path / 'ner', merged_path / 'ner')
 
+    merged_meta = merge_meta(json.load(open(tagger_path / 'meta.json')),
+                             json.load(open(ner_path / 'meta.json')))
+    json.dump(merged_meta, open(merged_path / 'meta.json', 'w'), indent=4)
+
+    open(merged_path / 'config.cfg', 'w').write(
+        merge_configs(open(tagger_path / 'config.cfg').read()))
+
+
+def merge_meta(tagger_meta, ner_meta):
     merged_meta = copy.deepcopy(tagger_meta)
-    merged_meta['pipeline'] = ['tagger', 'parser', 'ner']
-    merged_meta['accuracy']['ents_p'] = ner_meta['accuracy']['ents_p']
-    merged_meta['accuracy']['ents_r'] = ner_meta['accuracy']['ents_r']
-    merged_meta['accuracy']['ents_f'] = ner_meta['accuracy']['ents_f']
-    merged_meta['accuracy']['ents_per_type'] = ner_meta['accuracy']['ents_per_type']
+    merged_meta['pipeline'] = merged_meta['pipeline'] + ['ner']
+    merged_meta['components'] = merged_meta['components'] + ['ner']
+    merged_meta['performance']['ents_p'] = ner_meta['performance']['ents_p']
+    merged_meta['performance']['ents_r'] = ner_meta['performance']['ents_r']
+    merged_meta['performance']['ents_f'] = ner_meta['performance']['ents_f']
+    merged_meta['performance']['ents_per_type'] = ner_meta['performance']['ents_per_type']
     merged_meta['labels']['ner'] = ner_meta['labels']['ner']
+    return merged_meta
 
-    with open(os.path.join(merged_path, 'meta.json'), 'w') as f:
-        json.dump(merged_meta, f, indent=2)
 
-    shutil.copy(
-        os.path.join(tagger_path, 'tokenizer'),
-        os.path.join(merged_path, 'tokenizer')
-    )
-    shutil.copytree(
-        os.path.join(tagger_path, 'vocab'),
-        os.path.join(merged_path, 'vocab'))
-    shutil.copytree(
-        os.path.join(tagger_path, 'parser'),
-        os.path.join(merged_path, 'parser'))
-    shutil.copytree(
-        os.path.join(tagger_path, 'tagger'),
-        os.path.join(merged_path, 'tagger'))
-    shutil.copytree(
-        os.path.join(ner_path, 'ner'),
-        os.path.join(merged_path, 'ner'))
+def merge_configs(tagger_config):
+    config = tagger_config + """
+
+[components.ner]
+factory = "ner"
+incorrect_spans_key = null
+moves = null
+scorer = {"@scorers":"spacy.ner_scorer.v1"}
+update_with_oracle_cut_size = 100
+
+[components.ner.model]
+@architectures = "spacy.TransitionBasedParser.v2"
+state_type = "ner"
+extra_state_tokens = false
+hidden_width = 64
+maxout_pieces = 2
+use_upper = true
+nO = null
+
+[components.ner.model.tok2vec]
+@architectures = "spacy.Tok2VecListener.v1"
+width = ${components.tok2vec.model.encode.width}
+upstream = "*"
+"""
+
+    config = re.sub(r'(pipeline = \[.+)]', r'\1,"ner"]', config, re.MULTILINE)
+    return config
 
 
 if __name__ == '__main__':
-    main()
+    typer.run(main)
