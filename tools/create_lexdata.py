5cc4bbf35480bb6f81742b4a68ad4d387594e61f
Antti Ajanki <antti.ajanki@iki.fi>
2022-06-03 13:21:00 +0300

    limit the token maximum length in the lexdata

--- tools/create_lexdata.py
+++ tools/create_lexdata.py
@@ -23,23 +23,22 @@ def main(
             write_json_line(word_data, out)
 
 
-def read_freqs(full_loc, num_tokens):
+def read_freqs(full_loc, num_tokens, max_token_length=50):
     total = 0
     n = 0
     with gzip.open(full_loc, 'rt', encoding='utf-8') as f:
         for i, line in enumerate(f):
             n = i + 1
-            freq, token = line.strip().split(' ', 1)
-            freq = int(freq)
+            freq, _ = parse_token_line(line)
             total += freq
     log_total = math.log(total)
 
     probs = {}
     remaining_freq = total
     with gzip.open(full_loc, 'rt', encoding='utf-8') as f:
-        for line in islice(f, num_tokens):
-            freq, token = line.strip().split(' ', 1)
-            freq = int(freq)
+        parsed = (parse_token_line(x) for x in f)
+        valid_tokens = (x for x in parsed if len(x[1]) <= max_token_length)
+        for freq, token in islice(valid_tokens, num_tokens):
             probs[token] = math.log(freq) - log_total
             remaining_freq -= freq
 
@@ -48,6 +47,12 @@ def read_freqs(full_loc, num_tokens):
     oov_prob = math.log(remaining_freq) - log_total - math.log(n - len(probs))
 
     return probs, oov_prob
+
+
+def parse_token_line(x):
+    freq, token = x.strip().split(' ', 1)
+    freq = int(freq)
+    return freq, token
 
 
 def write_json_line(obj, fp):
