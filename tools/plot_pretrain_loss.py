89582372e69073c76d4bea732010586b6dbde5c5
Antti Ajanki <antti.ajanki@iki.fi>
2022-06-27 09:42:12 +0300

    plot only the final loss per epoch

--- tools/plot_pretrain_loss.py
+++ tools/plot_pretrain_loss.py
@@ -7,11 +7,15 @@ import json
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 
-epochs = []
+data = []
 for line in sys.stdin:
-    epochs.append(json.loads(line))
+    data.append(json.loads(line))
 
-df = pd.DataFrame(epochs).iloc[1:]
+df = pd.DataFrame(data)
+df = df.groupby('epoch').last().reset_index()
 df.plot(x='epoch', y='epoch_loss')
+loc = mticker.MaxNLocator(nbins='auto', integer=True)
+plt.gca().xaxis.set_major_locator(loc)
 plt.show()
